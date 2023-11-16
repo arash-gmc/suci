d470277209bbe9e8178535e58084c4f86decae13
arash-gmc <arash.green.423@gmail.com>
2023-11-16 12:01:01 +0000

    registearation functionality

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,16 +1,26 @@
 "use client";
 import { Box, Button, Flex, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { useForm } from "react-hook-form";
 
-const page = () => {
+const RegisterPage = () => {
   const fields = [
-    { label: "Name", value: "name" },
-    { label: "Email", value: "email" },
-    { label: "Password", value: "password" },
-    { label: "Repeat Passwor", value: "repeatPassword" },
+    { label: "Name", value: "name", type: "text" },
+    { label: "Email", value: "email", type: "email" },
+    { label: "Password", value: "password", type: "password" },
   ];
+
+  const { register, handleSubmit } = useForm();
+  const router = useRouter();
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post("/api/user", data);
+        router.push("/api/auth/signin");
+      })}
+    >
       <h1 className="text-center my-10">Register with Suci</h1>
       <Flex gap="5" direction="column" className="max-w-2xl mx-auto">
         {fields.map((field) => (
@@ -18,6 +28,8 @@ const page = () => {
             key={field.value}
             placeholder={field.label}
             size="3"
+            type={field.type}
+            {...register(field.value)}
           />
         ))}
         <Flex>
@@ -28,4 +40,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default RegisterPage;
