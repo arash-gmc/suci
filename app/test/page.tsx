f8d16f3d5e6406069259e558247ea6c1d22c370e
arash-gmc <arash.green.423@gmail.com>
2024-01-08 20:32:22 +0330

    fix a bug for deploy

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-import { Box, Flex } from "@radix-ui/themes";
-import React, { useState } from "react";
-import { BiLike } from "react-icons/bi";
+import React from "react";
 
 const page = () => {
-  const [click, setClick] = useState(false);
-  return (
-    <Flex m="6" py="7" justify="center" className="text-2xl">
-      <button
-        className={"btn " + (click ? "click" : "")}
-        onClick={() => {
-          setClick(true);
-          setTimeout(() => setClick(false), 2000);
-        }}
-      >
-        <BiLike />
-      </button>
-    </Flex>
-  );
+  return <div>page</div>;
 };
 
 export default page;
