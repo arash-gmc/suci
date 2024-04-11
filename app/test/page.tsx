4e1297ca5f0c462d634c6cb449422d0b58ff5c41
arash-gmc <arash.green.423@gmail.com>
2024-04-12 00:59:54 +0330

    bug fix

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,27 +1,5 @@
-"use client";
-import React, { useState } from "react";
-import { FaAmazon, FaReact } from "react-icons/fa6";
-import { motion } from "framer-motion";
-import { Flex } from "@radix-ui/themes";
-import { BiLike, BiSolidLike } from "react-icons/bi";
 const page = () => {
-  const [act, setAct] = useState(false);
-  const [liked, setLiked] = useState(false);
-  return (
-    <Flex align="center" justify="center" height="max-content" mt="9">
-      <motion.div
-        className="m-5 text-4xl"
-        onClick={() => {
-          setAct(true);
-          setLiked(!liked);
-          setTimeout(() => setAct(false), 600);
-        }}
-        animate={act ? { rotate: -10, scale: 1.1, y: -25 } : undefined}
-      >
-        {liked ? <BiSolidLike /> : <BiLike />}
-      </motion.div>
-    </Flex>
-  );
+  return <p>page</p>;
 };
 
 export default page;
