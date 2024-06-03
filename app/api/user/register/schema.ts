93e83847a9a9f8a34eed754b7e014dc8da5e33ea
arash-gmc <arash.green.423@gmail.com>
2024-06-03 09:53:53 +0330

    easier register

--- app/api/user/register/schema.ts
+++ app/api/user/register/schema.ts
@@ -3,17 +3,17 @@ import { z } from "zod";
 const usernameRegx = RegExp("^[a-zA-Z][a-zA-Z0-9_-]*$");
 
 export const newUserSchema = z.object({
-  name: z.string().min(5).max(32),
+  name: z.string().min(1).max(32),
   email: z.string().email(),
   username: z
     .string()
-    .min(3)
+    .min(1)
     .max(32)
     .regex(
       usernameRegx,
       "username characters must be english letter or numbers or _ and starts with a letter "
     ),
-  password: z.string().min(6, "password must be atleat 6 characters").max(255),
+  password: z.string().min(3, "password must be atleat 3 characters").max(255),
   city: z.string().optional(),
   brithYear: z.string().optional(),
   gender: z.enum(["male", "female"]).optional(),
